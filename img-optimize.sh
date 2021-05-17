a60c54260384b597e8cc6b1924b7a4c80fd65315
Rafał Kasprzyk <rafalkasprzyk@icloud.com>
2021-05-17 18:35:57 +0200

    Some changes to make it more flexible

--- img-optimize.sh
+++ img-optimize.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-for d in $(find ./uploads/ -maxdepth 2 -type d)
+while getopts p: flag
 do
-	echo $d
- 	for file in $d/*.jpeg; 
-	do 
-		if [ -f "$file" ]; then
-			#echo $file
-			magick $file -quality 90% -strip -resize 2050\> $file;
-		fi 
-	done	
+    case "${flag}" in
+        p) path=${OPTARG};;
+    esac
+done
+
+if [[ -z "$path" ]]; then
+    echo "You must provide path param" 1>&2
+    exit 1
+fi
+
+find $path -iname "*.jpg" -o -iname "*.jpeg" | while read file; do
+  echo "$file"
+  magick $file -quality 90 -strip -resize 2050\> $file;
 done
